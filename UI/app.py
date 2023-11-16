79a0fc87b9b70a52b092bf16802664cc08eb5690
dominiquebuford <97184076+dominiquebuford@users.noreply.github.com>
2023-11-16 16:42:40 -0500

    fixed frontpage images

--- UI/app.py
+++ UI/app.py
@@ -1,12 +1,50 @@
 import streamlit as st
 
 def page_home():
- 
+    # Set up HTML and CSS for split background
+    split_background_html = f'''
+        <style>
+            body {{
+                margin: 0;
+                padding: 0;
+            }}
+
+            .split-container {{
+                display: flex;
+                position:fixed;
+                height: 100vh; 
+                width: 100vw;
+                right: 0;
+                bottom: 0;
+            }}
+
+            .left-half, .right-half {{
+                flex: 1; /* Equal width for both halves */
+                background-size: cover; /* Ensure the background image covers the container */
+                background-position: center; /* Center the background image */
+            }}
+
+            .left-half {{
+                background-image: url('https://pbs.twimg.com/media/E46EJvSWYAsZR0C.jpg:large');
+            }}
+
+            .right-half {{
+                background-image:url('https://c8.alamy.com/comp/2MFMEEW/general-view-of-the-opening-kickoff-between-the-duke-blue-devils-and-the-north-carolina-tar-heels-in-an-empty-stadium-during-the-first-quarter-of-an-ncaa-college-football-game-at-wallace-wade-stadium-saturday-nov-7-2020-in-durham-nc-jim-dedmonpool-photo-via-ap-2MFMEEW.jpg')
+            }}
+        </style>
+        <div class="split-container">
+            <div class="left-half"></div>
+            <div class="right-half"></div>
+        </div>
+    '''
+
+#    Render the split background using st.markdown with unsafe_allow_html=True
+    st.markdown(split_background_html, unsafe_allow_html=True)
+
     duke_image_url = "https://pbs.twimg.com/profile_images/1629164833643048961/Vf2I35Mv_400x400.png"
     duke_image_html = f'''
-        <div style=" position: fixed; top: 3%; left:20%; width: 60%; text-align: center;">
-            <h1 align = "center"> Duke Athletics Attendance Prediction Model</h1>
-            <img src="{duke_image_url}" style="width: 30%; height: auto;">
+        <div style=" position: fixed; top: 10%; left:20%; width: 60%; text-align: center;">
+            <img src="{duke_image_url}" style="width: 40%; height: auto;">
         </div>
     '''
     st.markdown(duke_image_html, unsafe_allow_html=True)
@@ -17,7 +55,7 @@ def page_home():
 
     # Set up HTML and CSS to position the image at the bottom and make it wide
     crowd_image_html = f'''
-        <div style="position: fixed; bottom: 0; left: 0; width: 100%; text-align: center;">
+        <div style="position: fixed; bottom: -15%; left: 0; width: 100%; text-align: center;">
             <img src="{crowd_image_url}" style="width: 100%; height: auto;">
         </div>
     '''
