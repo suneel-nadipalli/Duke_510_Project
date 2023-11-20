8d501715b05efe7158a8266a130700e0c845f8fc
JaredBaileyDuke <141088319+JaredBaileyDuke@users.noreply.github.com>
2023-11-19 19:07:02 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+#####
+###
+# 00 Imports
+# 01 Pipelines
+###
+#####
+
+
+
+
+#####
+###
+# 00 Imports
+###
+#####
+from scripts.data_functions import *
+from scripts.model_functions import *
+
+
+
+
+
+#####
+###
+# 01 Pipelines
+###
+#####
+
+# data pipeline
+mf_train_df, mf_test_df, wm_train_df, wm_test_df = data_pipeline()
+
+# model pipeline
+mf_pred_df = model_pipeline(mf_train_df, mf_test_df)
+wb_pred_df = model_pipeline(wm_train_df, wm_test_df)
+
+# save data
+mf_pred_df.to_csv('data/mf_pred_df.csv', index=False)
+wb_pred_df.to_csv('data/wm_pred_df.csv', index=False)
